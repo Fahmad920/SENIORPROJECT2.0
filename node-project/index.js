aabab9ca33c42e21721877efd79a6e07524c7480
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2021-02-24 22:41:34 -0800

    Initial commit

---
+++ node-project/index.js
@@ -0,0 +1,8 @@
+const test = 'career cruising 2.0 XD';
+
+console.log(test);
+
+
+//good tutorial to watch is the node.js crash 
+//course tutorial by The Net Ninja. I think he does a good job at guiding through the 
+//steps of creating a website with node.js
