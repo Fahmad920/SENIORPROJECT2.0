27b1cc70442491fa61c8792edd172f669f8a5855
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2021-03-10 22:59:20 -0800

    3/10/21 setting up
    This is the basic code to set up a temporary localhost website for us to use to create the front end for now.

--- node-project/index.js
+++ node-project/index.js
@@ -1,8 +1,15 @@
-const test = 'career cruising 2.0 XD';
+const http = require('http');
 
-console.log(test);
+const server = http.createServer((req, res) =>{//creates server for us
+	console.log(req.url, req.method); //req = request being made 
+	//set header content type
+	res.setHeader('Content-Type', 'text/plain'); //setting the header to content type being sent back to the browser 
+	
+	res.write('hello'); //the content we want to send to the browser
+	res.end(); //ending the response which send it to the browser
+});  
 
+server.listen(3000, 'localhost', () =>{ //this function listens to the url on the local host on port 3000
+	console.log('listening for requests on port 3000');
+});
 
-//good tutorial to watch is the node.js crash 
-//course tutorial by The Net Ninja. I think he does a good job at guiding through the 
-//steps of creating a website with node.js
